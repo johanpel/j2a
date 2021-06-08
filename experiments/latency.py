ca7abc0113c6f0246e111f3ada1385d20753831e
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2021-06-08 07:57:49 -0400

    Skip useless experiments

--- experiments/latency.py
+++ experiments/latency.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 from experiment import Experiment
 import os
+import multiprocessing
 
-os.makedirs("data/latency/threads/metrics", exist_ok=True)
-os.makedirs("data/latency/threads/latency", exist_ok=True)
+os.makedirs("data/latency/threads/metrics/cpu", exist_ok=True)
+os.makedirs("data/latency/threads/latency/cpu", exist_ok=True)
+os.makedirs("data/latency/threads/metrics/fpga", exist_ok=True)
+os.makedirs("data/latency/threads/latency/fpga", exist_ok=True)
+
+repeats = 32
+experiments = []
 
 for n in range(0, 20, 1):
-    for t in range(1, 17):
-        Experiment(threads=min(t, 2 ** n),
-                   repeats=64,
-                   jsons=2 ** n,
-                   schema='io/battery.as',
-                   metrics="io/data/latency/threads/metrics/cpu",
-                   latency="io/data/latency/threads/latency/cpu").run()
+    for t in range(1, multiprocessing.cpu_count() + 1):
+        num_jsons = 2**n
+        if num_jsons >= t:
+            experiments.append(Experiment(threads=min(t, num_jsons),
+                       repeats=repeats,
+                       jsons=num_jsons,
+                       schema='/io/battery.as',
+                       metrics="/io/data/latency/threads/metrics/cpu",
+                       latency="/io/data/latency/threads/latency/cpu"))
 
-        Experiment(threads=min(t, 2 ** n),
-                   repeats=64,
-                   jsons=2 ** n,
-                   schema='io/battery.as',
-                   impl='opae-battery',
-                   metrics="io/data/latency/threads/metrics/fpga",
-                   latency="io/data/latency/threads/latency/fpga").run()
+            if t == 8:
+                experiments.append(Experiment(threads=min(t, num_jsons),
+                           repeats=repeats,
+                           jsons=num_jsons,
+                           schema='/io/battery.as',
+                           impl='opae-battery',
+                           metrics="/io/data/latency/threads/metrics/fpga",
+                           latency="/io/data/latency/threads/latency/fpga"))
+
+for e in experiments:
+    print(e)
+
+for e in experiments:
+    e.run()
--- experiments/throughput.py
+++ experiments/throughput.py
@@ -2,5 +2,5 @@
 from experiment import Experiment
 
 for t in range(1, 17):
-    Experiment(threads=t, repeats=8, jsons=1024 * 1024, schema='io/battery.as', metrics="io/data/throughput/threads/metrics",
-               latency="io/data/throughput/threads/latency").run()
+    Experiment(threads=t, repeats=8, jsons=1024 * 1024, schema='/io/battery.as', metrics="/io/data/throughput/threads/metrics",
+               latency="/io/data/throughput/threads/latency").run()
